3326aec51c534668517d495336cd54124d7f85fe
thabriele97 <gabriele.lagreca@gmail.com>
2022-05-24 00:41:50 +0200

    added folder DFX_Projects

---
+++ projects/DFX_Projects/cnt_mod16_wrapper.vhd
@@ -0,0 +1,121 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/20/2022 05:54:05 PM
+-- Design Name: 
+-- Module Name: cnt_mod16_wrapper - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cnt_mod16_wrapper is
+  PORT (
+    s00_axi_awaddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_awprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_awvalid : IN STD_LOGIC;
+    s00_axi_awready : OUT STD_LOGIC;
+    s00_axi_wdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_wstrb : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_wvalid : IN STD_LOGIC;
+    s00_axi_wready : OUT STD_LOGIC;
+    s00_axi_bresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_bvalid : OUT STD_LOGIC;
+    s00_axi_bready : IN STD_LOGIC;
+    s00_axi_araddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_arprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_arvalid : IN STD_LOGIC;
+    s00_axi_arready : OUT STD_LOGIC;
+    s00_axi_rdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_rresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_rvalid : OUT STD_LOGIC;
+    s00_axi_rready : IN STD_LOGIC;
+    s00_axi_aclk : IN STD_LOGIC;
+    s00_axi_aresetn : IN STD_LOGIC
+  );
+end cnt_mod16_wrapper;
+
+architecture Behavioral of cnt_mod16_wrapper is
+COMPONENT CNT_MOD16_0
+  PORT (
+    s00_axi_awaddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_awprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_awvalid : IN STD_LOGIC;
+    s00_axi_awready : OUT STD_LOGIC;
+    s00_axi_wdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_wstrb : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_wvalid : IN STD_LOGIC;
+    s00_axi_wready : OUT STD_LOGIC;
+    s00_axi_bresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_bvalid : OUT STD_LOGIC;
+    s00_axi_bready : IN STD_LOGIC;
+    s00_axi_araddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_arprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_arvalid : IN STD_LOGIC;
+    s00_axi_arready : OUT STD_LOGIC;
+    s00_axi_rdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_rresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_rvalid : OUT STD_LOGIC;
+    s00_axi_rready : IN STD_LOGIC;
+    s00_axi_aclk : IN STD_LOGIC;
+    s00_axi_aresetn : IN STD_LOGIC
+  );
+END COMPONENT;
+-- COMP_TAG_END ------ End COMPONENT Declaration ------------
+
+-- The following code must appear in the VHDL architecture
+-- body. Substitute your own instance name and net names.
+
+
+
+begin
+
+------------- Begin Cut here for INSTANTIATION Template ----- INST_TAG
+your_instance_name : CNT_MOD16_0
+  PORT MAP (
+    s00_axi_awaddr => s00_axi_awaddr,
+    s00_axi_awprot => s00_axi_awprot,
+    s00_axi_awvalid => s00_axi_awvalid,
+    s00_axi_awready => s00_axi_awready,
+    s00_axi_wdata => s00_axi_wdata,
+    s00_axi_wstrb => s00_axi_wstrb,
+    s00_axi_wvalid => s00_axi_wvalid,
+    s00_axi_wready => s00_axi_wready,
+    s00_axi_bresp => s00_axi_bresp,
+    s00_axi_bvalid => s00_axi_bvalid,
+    s00_axi_bready => s00_axi_bready,
+    s00_axi_araddr => s00_axi_araddr,
+    s00_axi_arprot => s00_axi_arprot,
+    s00_axi_arvalid => s00_axi_arvalid,
+    s00_axi_arready => s00_axi_arready,
+    s00_axi_rdata => s00_axi_rdata,
+    s00_axi_rresp => s00_axi_rresp,
+    s00_axi_rvalid => s00_axi_rvalid,
+    s00_axi_rready => s00_axi_rready,
+    s00_axi_aclk => s00_axi_aclk,
+    s00_axi_aresetn => s00_axi_aresetn
+  );
+  
+end Behavioral;
---
+++ projects/DFX_Projects/cnt_mod_wrapper2.vhd
@@ -0,0 +1,117 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/20/2022 03:58:29 PM
+-- Design Name: 
+-- Module Name: cnt_mod_wrapper2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cnt_mod_wrapper2 is
+  Port (
+    s00_axi_awaddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_awprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_awvalid : IN STD_LOGIC;
+    s00_axi_awready : OUT STD_LOGIC;
+    s00_axi_wdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_wstrb : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_wvalid : IN STD_LOGIC;
+    s00_axi_wready : OUT STD_LOGIC;
+    s00_axi_bresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_bvalid : OUT STD_LOGIC;
+    s00_axi_bready : IN STD_LOGIC;
+    s00_axi_araddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_arprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_arvalid : IN STD_LOGIC;
+    s00_axi_arready : OUT STD_LOGIC;
+    s00_axi_rdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_rresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_rvalid : OUT STD_LOGIC;
+    s00_axi_rready : IN STD_LOGIC;
+    s00_axi_aclk : IN STD_LOGIC;
+    s00_axi_aresetn : IN STD_LOGIC
+  );
+end cnt_mod_wrapper2;
+
+architecture Behavioral of cnt_mod_wrapper2 is
+COMPONENT CNT_MOD4_0
+  PORT (
+    s00_axi_awaddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_awprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_awvalid : IN STD_LOGIC;
+    s00_axi_awready : OUT STD_LOGIC;
+    s00_axi_wdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_wstrb : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_wvalid : IN STD_LOGIC;
+    s00_axi_wready : OUT STD_LOGIC;
+    s00_axi_bresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_bvalid : OUT STD_LOGIC;
+    s00_axi_bready : IN STD_LOGIC;
+    s00_axi_araddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    s00_axi_arprot : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    s00_axi_arvalid : IN STD_LOGIC;
+    s00_axi_arready : OUT STD_LOGIC;
+    s00_axi_rdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s00_axi_rresp : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    s00_axi_rvalid : OUT STD_LOGIC;
+    s00_axi_rready : IN STD_LOGIC;
+    s00_axi_aclk : IN STD_LOGIC;
+    s00_axi_aresetn : IN STD_LOGIC
+  );
+END COMPONENT;
+-- COMP_TAG_END ------ End COMPONENT Declaration ------------
+
+begin
+
+------------- Begin Cut here for INSTANTIATION Template ----- INST_TAG
+your_instance_name : CNT_MOD4_0
+  PORT MAP (
+    s00_axi_awaddr => s00_axi_awaddr,
+    s00_axi_awprot => s00_axi_awprot,
+    s00_axi_awvalid => s00_axi_awvalid,
+    s00_axi_awready => s00_axi_awready,
+    s00_axi_wdata => s00_axi_wdata,
+    s00_axi_wstrb => s00_axi_wstrb,
+    s00_axi_wvalid => s00_axi_wvalid,
+    s00_axi_wready => s00_axi_wready,
+    s00_axi_bresp => s00_axi_bresp,
+    s00_axi_bvalid => s00_axi_bvalid,
+    s00_axi_bready => s00_axi_bready,
+    s00_axi_araddr => s00_axi_araddr,
+    s00_axi_arprot => s00_axi_arprot,
+    s00_axi_arvalid => s00_axi_arvalid,
+    s00_axi_arready => s00_axi_arready,
+    s00_axi_rdata => s00_axi_rdata,
+    s00_axi_rresp => s00_axi_rresp,
+    s00_axi_rvalid => s00_axi_rvalid,
+    s00_axi_rready => s00_axi_rready,
+    s00_axi_aclk => s00_axi_aclk,
+    s00_axi_aresetn => s00_axi_aresetn
+  );
+-- INST_TAG_END ------ End INSTANTIATION Template ---------
+
+end Behavioral;
